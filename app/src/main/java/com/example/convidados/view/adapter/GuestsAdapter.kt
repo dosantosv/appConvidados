2882946f41b4f70ff84301b7429883e67a7ebb3d
Vinicius Santos <vdosantos12@hotmail.com>
2024-07-22 21:40:08 -0300

    Adds eventsListener for remove and click

--- app/src/main/java/com/example/convidados/view/adapter/GuestsAdapter.kt
+++ app/src/main/java/com/example/convidados/view/adapter/GuestsAdapter.kt
@@ -6,15 +6,17 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.convidados.databinding.RowGuestBinding
 import com.example.convidados.model.GuestModel
+import com.example.convidados.view.listener.OnGuestListener
 import com.example.convidados.view.viewholder.GuestViewHolder
 
 class GuestsAdapter : RecyclerView.Adapter<GuestViewHolder>() {
 
     private var guests: List<GuestModel> = listOf()
+    private lateinit var listener: OnGuestListener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GuestViewHolder {
         val item = RowGuestBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return GuestViewHolder(item)
+        return GuestViewHolder(item, listener)
     }
 
     override fun onBindViewHolder(holder: GuestViewHolder, position: Int) {
@@ -29,5 +31,9 @@ class GuestsAdapter : RecyclerView.Adapter<GuestViewHolder>() {
     fun updateGuests(list: List<GuestModel>) {
         guests = list
         notifyDataSetChanged()
+    }
+
+    fun attachListener(guestListener: OnGuestListener) {
+        listener = guestListener
     }
 }
--- app/src/main/java/com/example/convidados/view/viewholder/GuestViewHolder.kt
+++ app/src/main/java/com/example/convidados/view/viewholder/GuestViewHolder.kt
@@ -1,15 +1,25 @@
 package com.example.convidados.view.viewholder
 
-import android.widget.TextView
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.example.convidados.R
 import com.example.convidados.databinding.RowGuestBinding
 import com.example.convidados.model.GuestModel
+import com.example.convidados.view.listener.OnGuestListener
 
-class GuestViewHolder(private val bind: RowGuestBinding) : RecyclerView.ViewHolder(bind.root) {
+class GuestViewHolder(private val bind: RowGuestBinding, private val listener: OnGuestListener) :
+    RecyclerView.ViewHolder(bind.root) {
 
     fun bind(guest: GuestModel) {
         bind.textName.text = guest.name
+
+        bind.textName.setOnClickListener {
+            listener.onClick(guest.id)
+        }
+
+        bind.textName.setOnLongClickListener {
+            listener.onDelete(guest.id)
+            true
+        }
     }
 
 }
