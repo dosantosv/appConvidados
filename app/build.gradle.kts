d79bde1785f63b1a4a84082f42ec452beaca2840
Vinicius Santos <vdosantos12@hotmail.com>
2024-07-29 21:43:11 -0300

    use room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.utils.isKotlinKaptPluginApplied
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -40,6 +42,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.room:room-runtime:2.6.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
